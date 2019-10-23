b8be95e7bde492dcb95b23cbc6a9077d929520a2
Allen Pais <allen.lkml@gmail.com>
2019-10-23 15:05:51 +0530

    broadcom/b43legacy: Convert tasklets to use new tasklet_init API
    Switch to using the new tasklet_init() API which drops
    the unsigned long argument.
    
    Signed-off-by: Allen Pais <allen.lkml@gmail.com>

--- drivers/net/wireless/broadcom/b43legacy/main.c
+++ drivers/net/wireless/broadcom/b43legacy/main.c
@@ -1275,11 +1275,12 @@ static void handle_irq_ucode_debug(struct b43legacy_wldev *dev)
 }
 
 /* Interrupt handler bottom-half */
-static void b43legacy_interrupt_tasklet(struct b43legacy_wldev *dev)
+static void b43legacy_interrupt_tasklet(struct tasklet_struct *t)
 {
 	u32 reason;
-	u32 dma_reason[ARRAY_SIZE(dev->dma_reason)];
 	u32 merged_dma_reason = 0;
+	struct b43legacy_wldev *dev = from_tasklet(dev, t, isr_tasklet);
+	u32 dma_reason[ARRAY_SIZE(dev->dma_reason)];
 	int i;
 	unsigned long flags;
 
@@ -3740,9 +3741,7 @@ static int b43legacy_one_core_attach(struct ssb_device *dev,
 	wldev->wl = wl;
 	b43legacy_set_status(wldev, B43legacy_STAT_UNINIT);
 	wldev->bad_frames_preempt = modparam_bad_frames_preempt;
-	tasklet_init(&wldev->isr_tasklet,
-		     (void (*)(unsigned long))b43legacy_interrupt_tasklet,
-		     (unsigned long)wldev);
+	tasklet_init(&wldev->isr_tasklet, b43legacy_interrupt_tasklet);
 	if (modparam_pio)
 		wldev->__using_pio = true;
 	INIT_LIST_HEAD(&wldev->list);
